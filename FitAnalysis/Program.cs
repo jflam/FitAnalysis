ca651adbe3aca44368e50fbd04a69456f0a25992
John Lam <jlam@iunknown.com>
2014-12-20 15:54:21 -0800

    Initial commit of console project files and a skeleton command line options parser

---
+++ FitAnalysis/Program.cs
@@ -0,0 +1,37 @@
+﻿using CommandLine;
+using CommandLine.Text;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitAnalysis
+{
+    class Options
+    {
+        [Option('d', "dir", Required = true, HelpText = "Directory to process")]
+        public string Directory { get; set; }
+
+        [ParserState]
+        public IParserState LastParserState { get; set; }
+
+        [HelpOption]
+        public string GetUsage()
+        {
+            return HelpText.AutoBuild(this,
+                (HelpText current) => HelpText.DefaultParsingErrorsHandler(this, current));
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var options = new Options();
+            if (Parser.Default.ParseArguments(args, options))
+            {
+                Console.WriteLine("Parsing {0}", options.Directory);
+            }
+        }
+    }
+}
