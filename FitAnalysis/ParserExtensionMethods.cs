ef71c07c24d1aa61f3915699877ab555a3f362b5
John Lam <jlam@iunknown.com>
2015-01-01 12:09:28 -0800

    Adding the extension methods file

---
+++ FitAnalysis/ParserExtensionMethods.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using FastFitParser.Core;
+
+namespace FitAnalysis
+{
+    public static class ParserExtensionMethods
+    {
+        public static bool IsStopTimerEvent(this DataRecord record)
+        {
+            if (record.GlobalMessageNumber == GlobalMessageNumber.Event)
+            {
+                byte eventField, eventTypeField;
+                if (record.TryGetField((byte)EventFieldNumber.Event, out eventField))
+                {
+                    if ((Event)eventField == Event.Timer)
+                    {
+                        if (record.TryGetField((byte)EventFieldNumber.EventType, out eventTypeField))
+                        {
+                            EventType eventType = (EventType)eventTypeField;
+                            if (eventType == EventType.Stop || eventType == EventType.StopAll)
+                            {
+                                return true;
+                            }
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
